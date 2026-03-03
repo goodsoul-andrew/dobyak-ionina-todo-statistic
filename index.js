0f33d8457a7e5433bffd910abdd2f61af08c0a4e
Andrew Goodman <goodsoul.andrew@mail.ru>
2026-03-03 19:40:53 +0500

    доделал

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
-const {readLine} = require('./console');
+const { getAllFilePathsWithExtension, readFile } = require("./fileSystem");
+const { readLine } = require("./console");
 
 const files = getFiles();
-const filesTodo = files.map((file) => {
-    return file.match(/\/\/ TODO\s+.*\n/g) || [];
-})
+const filesTodoRaw = files.map((file) => {
+  return file.match(/\/\/ TODO\s+.*\n/g) || [];
+});
+const filesTodo = filesTodoRaw.map(arr => arr.map((el) => el.slice(8, el.length - 1)));
 console.log(filesTodo);
 
-console.log('Please, write your command!');
+console.log("Please, write your command!");
 readLine(processCommand);
 
 function getFiles() {
-    const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
-    return filePaths.map(path => readFile(path));
+  const filePaths = getAllFilePathsWithExtension(process.cwd(), "js");
+  return filePaths.map((path) => readFile(path));
 }
 
 function processCommand(command) {
-    switch (command) {
-        case 'exit':
-            process.exit(0);
-            break;
-        default:
-            console.log('wrong command');
-            break;
-    }
+  switch (command) {
+    case "exit":
+      process.exit(0);
+      break;
+    default:
+      console.log("wrong command");
+      break;
+  }
 }
 
 // TODO you can do it!
